74641ccfc3411ae3677175fd0cd9c36aad694e44
wuyu0107 <mkim050107@gmail.com>
2025-03-12 23:12:22 +0700

    [REFACTOR] Optimize method joinStudentNames()

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -54,11 +54,7 @@ public class StudentService {
 
     public String joinStudentNames() {
         List<Student> students = studentRepository.findAll();
-        String result = "";
-        for (Student student : students) {
-            result += student.getName() + ", ";
-        }
-        return result.substring(0, result.length() - 2);
+        return students.stream().map(Student::getName).collect(Collectors.joining(", "));
     }
 }
 
