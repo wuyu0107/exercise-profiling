aeec9af4524b9d5748d85725189482304245eac8
wuyu0107 <mkim050107@gmail.com>
2025-03-12 23:07:16 +0700

    [REFACTOR] Optimizing method getAllStudentWithCourse

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author muhammad.khadafi
@@ -25,16 +28,14 @@ public class StudentService {
 
     public List<StudentCourse> getAllStudentsWithCourses() {
         List<Student> students = studentRepository.findAll();
-        List<StudentCourse> studentCourses = new ArrayList<>();
-        for (Student student : students) {
-            List<StudentCourse> studentCoursesByStudent = studentCourseRepository.findByStudentId(student.getId());
-            for (StudentCourse studentCourseByStudent : studentCoursesByStudent) {
-                StudentCourse studentCourse = new StudentCourse();
-                studentCourse.setStudent(student);
-                studentCourse.setCourse(studentCourseByStudent.getCourse());
-                studentCourses.add(studentCourse);
-            }
-        }
+        List<StudentCourse> studentCourses = studentCourseRepository.findAll();
+
+        Map<Long, Student> studentMap = students.stream().collect(Collectors.toMap(Student::getId, Function.identity()));
+
+        studentCourses.forEach(sc -> {
+            Student student = studentMap.get(sc.getStudent().getId());
+            sc.setStudent(student);
+        });
         return studentCourses;
     }
 
